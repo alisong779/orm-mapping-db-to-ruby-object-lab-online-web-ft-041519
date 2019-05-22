11c1468692ad09b054e0509b7386c05e9919d010
Alison Golden <alisong779@yahoo.com>
2019-05-22 15:19:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,7 +82,7 @@ class Student
     end 
   end 
   
-  def self.first_x_students_in_grade_10(number)
+  def self.first_X_students_in_grade_10(number)
     sql = <<-SQL 
       SELECT * FROM students
       WHERE grade = 10
