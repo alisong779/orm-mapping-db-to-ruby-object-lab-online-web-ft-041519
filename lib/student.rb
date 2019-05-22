9bb438c0e94e3822c90a7cd173e02a9240a2c856
Alison Golden <alisong779@yahoo.com>
2019-05-22 14:56:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,6 +66,14 @@ class Student
   end 
   
   def self.all_students_below_12th_grade
+      sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.grade < 12 
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
   end 
   
 end
