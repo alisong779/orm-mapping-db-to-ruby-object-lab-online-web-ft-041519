3fe3cbce6cff5deb194ae1007acc160b28baf938
Alison Golden <alisong779@yahoo.com>
2019-05-22 15:21:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,10 +94,19 @@ class Student
       end 
   end
 
-    
- 
-  
+    def self.all_students_in_grade_X(grade_num)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.grade = ?
+      LIMIT 1 
+    SQL
 
+    DB[:conn].execute(sql, grade_num).map do |row|
+      self.new_from_db(row)
+    end 
+  end 
+ 
   def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT *
